b3be76155b161f2ebf270429c7c2f2c4da1be9d6
adrienraynaudd <adrien48632@gmail.com>
2024-04-23 11:24:03 +0200

    home

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent} from './auth/login/login.component';
+import { HomeComponent } from './home/home/home.component';
 
 export const routes: Routes = [
-    //{ path: '', component: AccueilComponent },
+    { path: '', component: HomeComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent},
     { path: '**', redirectTo: '' }
 ];
